2390f0a3be434b3b5e68f8855aa7d3eca3a8acac
Jonas Boone <jonas.boone@student.kdg.be>
2021-08-04 18:59:32 +0200

    Added first UI and classes to generate random number and insert into DOM

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,50 @@
 // import styling
 import './css/custom.scss'
+
+class UserInterfaceComponents {
+    rollBtn: HTMLButtonElement;
+
+    constructor() {
+        this.rollBtn = document.getElementById("roll-btn")! as HTMLButtonElement;
+    }
+}
+
+const userInterfaceComponents = new UserInterfaceComponents();
+
+
+// class for game input
+class PlayerInteractions {
+    constructor() {
+        // click event on roll dice button
+        userInterfaceComponents.rollBtn.addEventListener("click", () => {
+            scoreCalculations.insertCurrentDiceRollScore();
+        })
+    }
+
+    // method to generate random number on click
+    generateRandomNumber() {
+        return Math.trunc(Math.random() * 6 + 1);
+    }
+}
+
+// @ts-ignore
+const playerInteraction = new PlayerInteractions()
+
+// class for scores
+class ScoreCalculations {
+    currentDiceRollScoreContainer: HTMLDivElement;
+
+    constructor() {
+        this.currentDiceRollScoreContainer = document.getElementById("current-dice-roll-score")! as HTMLDivElement;
+    }
+
+    // method to insert number of eyes into DOM
+    insertCurrentDiceRollScore() {
+        const currentDiceRollScore = playerInteraction.generateRandomNumber();
+        this.currentDiceRollScoreContainer.innerText = currentDiceRollScore.toString();
+    }
+
+}
+
+// @ts-ignore
+const scoreCalculations = new ScoreCalculations();
