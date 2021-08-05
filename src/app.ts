5a6bb4c8fe634e6b4d2a9872430f0c1e7f022ef6
Jonas Boone <jonas.boone@student.kdg.be>
2021-08-05 16:47:49 +0200

    Final version

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,18 @@ import './css/custom.scss'
 
 // class for user interface components
 class UserInterfaceComponents {
-    rollDiceBtn: HTMLButtonElement;
     currentDiceRollContainer: HTMLDivElement;
     currentScorePlayer1: HTMLParagraphElement;
     currentScorePlayer2: HTMLParagraphElement;
-    newGameBtn: HTMLButtonElement;
+    rollBtn: HTMLButtonElement;
+    newBtn: HTMLButtonElement;
 
     constructor() {
-        this.rollDiceBtn = document.getElementById("roll-btn")! as HTMLButtonElement;
+        this.rollBtn = document.getElementById("roll-btn")! as HTMLButtonElement;
         this.currentDiceRollContainer = document.getElementById("current-dice-roll-score")! as HTMLDivElement;
-        this.currentScorePlayer1 = document.getElementById("current--0")!  as HTMLParagraphElement;
-        this.currentScorePlayer2 = document.getElementById("current--1")!  as HTMLParagraphElement;
-        this.newGameBtn = document.getElementById("new-btn")! as HTMLButtonElement;
+        this.currentScorePlayer1 = document.getElementById("current--1")!  as HTMLParagraphElement;
+        this.currentScorePlayer2 = document.getElementById("current--2")!  as HTMLParagraphElement;
+        this.newBtn = document.getElementById("new-btn")! as HTMLButtonElement;
     }
 }
 
@@ -63,27 +63,38 @@ class ScoreCalculations {
 
         } else {
             // else add number of eyes to score
-            this.addScoreToCurrentScore(dice)
+            this.calculateCurrentScore(dice)
         }
     }
 
     // add score and insert in DOM
-    addScoreToCurrentScore(dice: number) {
+    calculateCurrentScore(dice: number) {
         this.currentScore = this.currentScore + dice;
 
         if (players.determineCurrentPlayer() === 'Player-1') {
             userInterface.currentScorePlayer1.innerHTML = this.currentScore.toString();
+            // check if won
+            this.checkIfWon(this.currentScore, 'Player-1');
     } else {
             userInterface.currentScorePlayer2.innerHTML = this.currentScore.toString();
+            // check if won
+            this.checkIfWon(this.currentScore, 'Player-2');
         }
     }
 
-    // determine winner
-    determineWinner() {
-        if (players.determineCurrentPlayer() === 'Player-1' && this.currentScore >= 100) {
+    checkIfWon(score: number, player: string) {
+        if (score >= 10 && player === 'Player-1') {
             players.player1.classList.add('player--winner');
-        } else if (players.determineCurrentPlayer() === 'Player-2' && this.currentScore >= 100) {
-            players.player2.classList.add('player-winner');
+            userInterface.currentDiceRollContainer.classList.add('hidden')
+            userInterface.rollBtn.classList.add('hidden');
+            players.namePlayer1.innerHTML += ' has won!'
+
+        } else if (score >= 10 && player === 'Player-2') {
+            players.player2.classList.add('player--winner');
+            userInterface.currentDiceRollContainer.classList.add('hidden')
+            players.namePlayer2.innerHTML += ' has won!';
+            userInterface.rollBtn.classList.add('hidden');
+
         }
     }
 }
@@ -92,10 +103,14 @@ class ScoreCalculations {
 class Player {
     player1: HTMLElement;
     player2: HTMLElement;
+    namePlayer1: HTMLTitleElement;
+    namePlayer2: HTMLTitleElement;
 
     constructor() {
-        this.player1 = document.getElementById("player--0")! as HTMLElement;
-        this.player2 = document.getElementById("player--1")! as HTMLElement;
+        this.player1 = document.getElementById("player--1")! as HTMLElement;
+        this.player2 = document.getElementById("player--2")! as HTMLElement;
+        this.namePlayer1 = document.getElementById("name--1")! as HTMLTitleElement;
+        this.namePlayer2 = document.getElementById("name--2")! as HTMLTitleElement;
     }
 
     determineCurrentPlayer() {
@@ -122,13 +137,13 @@ const scoreCalculations = new ScoreCalculations();
 class eventListeners {
     constructor() {
         // click event on roll dice button
-        userInterface.rollDiceBtn.addEventListener("click", () => {
+        userInterface.rollBtn.addEventListener("click", () => {
             // on each click calculate the current score
             scoreCalculations.updateCurrentPlayerScore();
         })
 
         // click event on new game button
-        userInterface.newGameBtn.addEventListener("click", () => {
+        userInterface.newBtn.addEventListener("click", () => {
             // reset scores
             userInterface.currentScorePlayer1.innerHTML = '0';
             userInterface.currentScorePlayer2.innerHTML = '0';
@@ -142,6 +157,18 @@ class eventListeners {
 
             // reset dice
             userInterface.currentDiceRollContainer.innerHTML = '';
+            userInterface.currentDiceRollContainer.classList.remove('hidden');
+
+            // reset winner class
+            players.player1.classList.remove('player--winner');
+            players.player2.classList.remove('player--winner');
+
+            // reset text
+            players.namePlayer1.innerHTML = 'Player 1';
+            players.namePlayer2.innerHTML = 'Player 2';
+
+            // show roll dice button
+            userInterface.rollBtn.classList.remove('hidden');
         })
     }
 }
